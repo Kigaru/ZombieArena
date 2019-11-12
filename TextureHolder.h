c6e11611c3edcde69aaa2c371629ff00a1630fd0
Kigaru <jakubster9@gmail.com>
2019-11-12 11:51:18 +0000

    Created a texture holder class

---
+++ TextureHolder.h
@@ -0,0 +1,22 @@
+#ifndef textureHolder_h
+#define textureHolder_h
+
+#include <SFML/Graphics.hpp>
+#include <map>
+
+using namespace sf;
+using namespace std;
+class TextureHolder
+{
+public:
+	TextureHolder();
+	static Texture& GetTexture(string const& filename);
+private:
+	// a map holding pairs of string + texture
+	map<string, Texture> m_Textures;
+
+	static TextureHolder* m_s_Instance;
+};
+
+
+#endif // !1
